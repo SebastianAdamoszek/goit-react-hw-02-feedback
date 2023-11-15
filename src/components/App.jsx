d4a4c1250bc4a33b59db46c4c045212df663714a
Seba <sebaadamoszek@vp.pl>
2023-11-15 19:21:46 +0100

    task feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import React, { useState } from 'react';
+import Section from './SectionTitle/SectionTitle';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+// import Feedback from './Feedback/Feedback';
+
+const App = () => {
+  const [feedbackState, setFeedbackState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = type => {
+    setFeedbackState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  const calculateTotalFeedback = () => {
+    return feedbackState.good + feedbackState.neutral + feedbackState.bad;
+  };
+
+  const calculatePositivePercentage = () => {
+    const totalFeedback = calculateTotalFeedback();
+    return totalFeedback > 0
+      ? ((feedbackState.good / totalFeedback) * 100).toFixed(2)
+      : 0;
+  };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
-      React homework template
+      <Section title="Feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        <Statistics
+          good={feedbackState.good}
+          neutral={feedbackState.neutral}
+          bad={feedbackState.bad}
+          total={calculateTotalFeedback()}
+          positivePercentage={calculatePositivePercentage()}
+        />
+      </Section>
     </div>
   );
 };
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option.charAt(0).toUpperCase() + option.slice(1)}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
